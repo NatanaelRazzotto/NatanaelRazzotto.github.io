bac2c575fe4bd1d92d87ea4bb8c48f6516e821c0
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2023-03-11 20:47:46 -0300

    Carrosel base interface

--- script.js
+++ script.js
@@ -8,6 +8,30 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
            console.log('dsad')
         }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHouverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+            },
+            600:{
+                items:2,
+                nav:false,
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
 
-    })
 });
